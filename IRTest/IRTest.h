12318e7d39fed2544be6c5d3051b92096a500941
Phil Chang <phil.chang@yahooinc.com>
2023-07-14 18:43:52 +0800

    Initial Commit

---
+++ IRTest/IRTest.h
@@ -0,0 +1,19 @@
+//
+//  IRTest.h
+//  IRTest
+//
+//  Created by Phil Chang on 2023/7/14.
+//  Copyright © 2023 Yahoo. All rights reserved.
+//        
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IRTest.
+FOUNDATION_EXPORT double IRTestVersionNumber;
+
+//! Project version string for IRTest.
+FOUNDATION_EXPORT const unsigned char IRTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IRTest/PublicHeader.h>
+
+
